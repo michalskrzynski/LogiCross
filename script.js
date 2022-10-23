34c5f0b18e996470cb93d90c39ba208030796fcd
michalskrzynski <michal.skrzynski@gmail.com>
2022-10-23 12:07:06 +0200

    switch difficulty

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const rulesPopup = document.getElementById( 'rulesPopup' );
 const howToPlay = document.getElementById( 'howToPlay' );
 howToPlay.addEventListener( 'click', e => rulesPopup.style.display = 'block' );
 
+const gameEasy = document.getElementById( 'gameEasy' );
+const gameHard = document.getElementById( 'gameHard' );
+const undo = document.getElementById( 'undo' );
+
+gameEasy.addEventListener( 'click', e=> { 
+  gameEasy.style.textDecoration = 'underline'; 
+  gameHard.style.textDecoration = 'none';
+  undo.style.visibility = 'visible'; 
+});
+gameHard.addEventListener( 'click', e=> { 
+  gameEasy.style.textDecoration = 'none'; 
+  gameHard.style.textDecoration = 'underline';
+  undo.style.visibility = 'hidden'; 
+});
 
 let game = new Game(levels, 15);
 game.restart();
